22d15d2bdddd4230f051dccb5e1dce1c86652a9b
lorna <lornamuringi@gmail.com>
2026-02-26 11:25:03 +0300

    Added .env file and a function for Home Page

--- En-Router/src/pages/Home.jsx
+++ En-Router/src/pages/Home.jsx
@@ -1,1 +1,13 @@
-const [destinations, setDestinations] = useState([]);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+export default App;
